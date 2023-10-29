20842c7e07ca2a08bc871288f0b66f601ee87755
CPU <601130640@qq.com>
2023-10-29 23:15:51 +0800

    添加密码加密

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -40,7 +40,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        String Md5Password = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!Md5Password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
